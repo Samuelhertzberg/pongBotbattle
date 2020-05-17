c9abb4dac9ba87960fe092499a7ae468aa682813
Samuel <shert@kth.se>
2020-05-17 22:32:25 +0200

    Tally counter and edge case fixes

--- graphicsHandler.py
+++ graphicsHandler.py
@@ -10,17 +10,19 @@ windowWidth = 1000
 windowHeight = 800
 
 ballPos = [windowWidth/2, windowHeight/2]
-ballVec = [2,0.15]
+ballVec = [-2,1]
 ballRadius = 10
 playerMargin = 50
 playerDimensions = [10,100]
 p1Pos = [playerMargin + playerDimensions[0]/2, windowHeight/2]
 p2Pos = [windowWidth - playerMargin - playerDimensions[0]/2, windowHeight/2]
 
-playerSpeed = 5
+playerSpeed = 1
 
 p1Move = [0]
 p2Move = [0]
+
+tally = [0,0]
 
 deflectionFactor = 10
 
@@ -31,7 +33,8 @@ win = GraphWin('PONG', windowWidth, windowHeight)  # give title and dimensions
 win.setBackground("black")
 
 p1Rect = Rectangle(
-    Point(p1Pos[0] - playerDimensions[0], p1Pos[1] - (playerDimensions[1] / 2)),
+    Point(p1Pos[0] - playerDimensions[0],
+            p1Pos[1] - (playerDimensions[1] / 2)),
     Point(p1Pos[0], p1Pos[1] + (playerDimensions[1] / 2))
 )
 p2Rect = Rectangle(
@@ -41,12 +44,20 @@ p2Rect = Rectangle(
 ball = Circle(
     Point(ballPos[0], ballPos[1]), ballRadius
 )
+textP1 = Text(Point(windowWidth*0.25, 50), tally[0])
+textP2 = Text(Point(windowWidth*0.75, 50), tally[1])
+
 p1Rect.setFill("white")
 p2Rect.setFill("white")
 ball.setFill("white")
+textP1.setTextColor("White")
+textP2.setTextColor("White")
+
 p1Rect.draw(win)
 p2Rect.draw(win)
 ball.draw(win)
+textP1.draw(win)
+textP2.draw(win)
 
 def playerCollision(playerPos):
     xDist = abs(ballPos[0] - playerPos[0]) - ballRadius
@@ -61,7 +72,6 @@ def playerCollision(playerPos):
     sign = (1, -1)[ballPos[1] - playerPos[1] < 0]
     deflection *= sign
     ballVec[1] += deflection
-    
 
 def updateBall():
     ballPos[0] += ballVec[0]
@@ -69,9 +79,11 @@ def updateBall():
     ball.move(ballVec[0], ballVec[1])
 
     if ballPos[1] - ballRadius <= 0:
-        ballVec[1] *= -1 
+        ballVec[1] *= -1
+        ballPos[1] = 0 + ballRadius 
     if ballPos[1] + ballRadius >= windowHeight:
         ballVec[1] *= -1
+        ballPos[1] = windowHeight - ballRadius 
     playerCollision(p1Pos)
     playerCollision(p2Pos)
     if(ballPos[0] < 0 or ballPos[0] > windowWidth):
@@ -79,26 +91,27 @@ def updateBall():
     return True
 
 def moveP1(direction):
-    p1MoveRaw = p2Bot.move(ballPos, ballVec, p1Pos, p2Pos, playerSpeed)
-    p1Move[0] = makeLegal(p1MoveRaw, p2Pos)
-    
+    p1MoveRaw = p1Bot.move(ballPos, ballVec, ballRadius, p1Pos, p2Pos, playerSpeed, windowWidth, windowHeight)
+    p1Move[0] = makeLegal(p1MoveRaw, p1Pos)
+
 def moveP2(direction):
-    p2MoveRaw = p2Bot.move(ballPos, ballVec, p2Pos, p1Pos, playerSpeed)
+    p2MoveRaw = p2Bot.move(ballPos, ballVec, ballRadius, p2Pos, p1Pos, playerSpeed, windowWidth, windowHeight)
     p2Move[0] = makeLegal(p2MoveRaw, p2Pos)
 
-
 def makeLegal(move, playerPos):
-    if(move == None):
-        return 0
-    if(move < 0 and playerPos[1] + move - playerDimensions[1]/2 < 0):
-        return 0
-    if(move > 0 and playerPos[1] + move + playerDimensions[1]/2 > windowHeight):
-        return 0
+    legalMove = 0
     if(move > playerSpeed):
-        return playerSpeed
+        legalMove =  playerSpeed
     if(move < -playerSpeed):
-        return -playerSpeed
-    return move
+        legalMove =  -playerSpeed
+
+    if(legalMove == None):
+        legalMove =  0
+    if(legalMove < 0 and playerPos[1] + legalMove - playerDimensions[1]/2 < 0):
+        legalMove =  0
+    if(legalMove > 0 and playerPos[1] + legalMove + playerDimensions[1]/2 > windowHeight):
+        legalMove =  0
+    return legalMove
 
 
 def updatePlayer1():
@@ -109,10 +122,23 @@ def updatePlayer2():
     p2Pos[1] += p2Move[0]
     p2Rect.move(0,p2Move[0])
 
-while updateBall():
-    moveP1(ballPos[1] - p1Pos[1] - 50)
-    moveP2(ballPos[1] - p2Pos[1] - 50)
-    updatePlayer1()
-    updatePlayer2()
-    sleep(1/goalfps)
+while (sum(tally) < 10) or tally[0] == tally[1]:
+    ballVec = [2*(1,-1)[sum(tally) % 2 == 0], 0]
+    while updateBall():
+        moveP1(ballPos[1] - p1Pos[1] - 50)
+        moveP2(ballPos[1] - p2Pos[1] - 50)
+        updatePlayer1()
+        updatePlayer2()
+        # sleep(1/goalfps)
+    tally[(windowWidth - ballPos[0]) > ballPos[0]] += 1
+    textP1.setText(tally[0])
+    textP2.setText(tally[1])
+    ##Reset game
+    ball.move(windowWidth/2 - ballPos[0], windowHeight/2 - ballPos[1])
+    ballPos = [windowWidth/2, windowHeight/2]
+    p1Rect.move(0, windowHeight/2 - p1Pos[1])
+    p2Rect.move(0, windowHeight/2 - p2Pos[1])
+    p1Pos[1] = p2Pos[1] = windowHeight/2
+print("GAME OVER")
+print("Player %s won!" % ("1" if tally[0] > tally[1] else "2"))
 win.close()
