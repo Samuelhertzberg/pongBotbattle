c8d8b3b83e1b9db63a63fa9070fc4aed41e75819
Samuel <shert@kth.se>
2020-05-18 14:37:08 +0200

    One bugfix and mine changes for testing

--- graphicsHandler.py
+++ graphicsHandler.py
@@ -10,14 +10,15 @@ windowWidth = 1000
 windowHeight = 800
 
 ballPos = [windowWidth/2, windowHeight/2]
-ballVec = [-2,1]
+# ballPos = [900, 700]
+ballVec = [-2,-10]
 ballRadius = 10
 playerMargin = 50
 playerDimensions = [10,100]
 p1Pos = [playerMargin + playerDimensions[0]/2, windowHeight/2]
 p2Pos = [windowWidth - playerMargin - playerDimensions[0]/2, windowHeight/2]
 
-playerSpeed = 1
+playerSpeed = 5
 
 p1Move = [0]
 p2Move = [0]
@@ -27,7 +28,7 @@ tally = [0,0]
 deflectionFactor = 10
 
 gameOver = False
-goalfps = 600
+goalfps = 60
 
 win = GraphWin('PONG', windowWidth, windowHeight)  # give title and dimensions
 win.setBackground("black")
@@ -99,18 +100,17 @@ def moveP2(direction):
     p2Move[0] = makeLegal(p2MoveRaw, p2Pos)
 
 def makeLegal(move, playerPos):
-    legalMove = 0
+    legalMove = move
     if(move > playerSpeed):
         legalMove =  playerSpeed
-    if(move < -playerSpeed):
+    elif(move < -playerSpeed):
         legalMove =  -playerSpeed
-
     if(legalMove == None):
         legalMove =  0
     if(legalMove < 0 and playerPos[1] + legalMove - playerDimensions[1]/2 < 0):
-        legalMove =  0
+        legalMove =  - (playerPos[1] - playerDimensions[1]/2)
     if(legalMove > 0 and playerPos[1] + legalMove + playerDimensions[1]/2 > windowHeight):
-        legalMove =  0
+        legalMove =  windowHeight - (playerPos[1] + playerDimensions[1]/2)
     return legalMove
 
 
@@ -123,13 +123,13 @@ def updatePlayer2():
     p2Rect.move(0,p2Move[0])
 
 while (sum(tally) < 10) or tally[0] == tally[1]:
-    ballVec = [2*(1,-1)[sum(tally) % 2 == 0], 0]
+    ballVec = [2*(1,-1)[sum(tally) % 2 == 0], 10]
     while updateBall():
         moveP1(ballPos[1] - p1Pos[1] - 50)
         moveP2(ballPos[1] - p2Pos[1] - 50)
         updatePlayer1()
         updatePlayer2()
-        # sleep(1/goalfps)
+        sleep(1/goalfps)
     tally[(windowWidth - ballPos[0]) > ballPos[0]] += 1
     textP1.setText(tally[0])
     textP2.setText(tally[1])
